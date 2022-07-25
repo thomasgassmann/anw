f06cf19075b4c649905fe19c03c23d4ef329ca5b
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-07-26 00:14:50 +0200

    Add slay the dragon
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- src/slay_the_dragon/SlayTheDragon.java
+++ src/slay_the_dragon/SlayTheDragon.java
@@ -5,12 +5,13 @@ import io.Out;
 import org.junit.jupiter.api.Test;
 
 import java.text.DecimalFormat;
+import java.util.HashMap;
 
 public class SlayTheDragon {
     @Test
     public void main() {
-        In.open("src/slay_the_dragon/public/test1.in");
-        Out.compareTo("src/slay_the_dragon/public/test1.out");
+        In.open("src/slay_the_dragon/public/test2.in");
+        Out.compareTo("src/slay_the_dragon/public/test2.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -19,6 +20,8 @@ public class SlayTheDragon {
     }
 
     private static void testCase() {
+        memo = new HashMap<>();
+
         int n = In.readInt(); // health points dragon
         int f = In.readInt(); // frost power
         int s = In.readInt(); // shadow power
@@ -35,13 +38,43 @@ public class SlayTheDragon {
         }
     }
 
+    private static HashMap<Integer, HashMap<Integer, HashMap<Integer, Double>>> memo = new HashMap<>();
+
+    private static boolean has(int n, int f, int s) {
+        return memo.containsKey(n) && memo.get(n).containsKey(f) && memo.get(n).get(f).containsKey(s);
+    }
+
+    private static double get(int n, int f, int s) {
+        return memo.get(n).get(f).get(s);
+    }
+
+    private static void memoize(int n, int f, int s, double value) {
+        if (!memo.containsKey(n)) {
+            memo.put(n, new HashMap<>());
+        }
+
+        if (!memo.get(n).containsKey(f)) {
+            memo.get(n).put(f, new HashMap<>());
+        }
+
+        memo.get(n).get(f).put(s, value);
+    }
+
     private static double expectedValue(int n, int f, int s, int x, int y) {
+        if (f * x + s * y < n) {
+            return Double.NaN;
+        }
+
         if (n <= 0) {
             return 0;
         }
 
         if (n <= x && n <= y) {
             return 1;
+        }
+
+        if (has(n, f, s)) {
+            return get(n, f, s);
         }
 
         double frost = ((double)f / (f + s));
@@ -55,6 +88,8 @@ public class SlayTheDragon {
             total += shadow * (1 + expectedValue(n - y, f, s - 1, x, y));
         }
 
-        return total == 0 ? Double.NaN : total;
+        double res = total == 0 ? Double.NaN : total;
+        memoize(n, f, s, res);
+        return res;
     }
 }
