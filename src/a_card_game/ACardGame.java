514cee28893c2c28f39b5af0622f53b24ccdc6a8
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-07-28 01:11:41 +0200

    A card game
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- src/a_card_game/ACardGame.java
+++ src/a_card_game/ACardGame.java
@@ -1,14 +1,18 @@
 package a_card_game;
 
+import algorithms.Algorithms;
 import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class ACardGame {
     @Test
     public void main() {
-        In.open("src/a_card_game/public/test2.in");
-        Out.compareTo("src/a_card_game/public/test2.out");
+        In.open("src/a_card_game/public/test1.in");
+        Out.compareTo("src/a_card_game/public/test1.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -17,16 +21,25 @@ public class ACardGame {
     }
 
     public static void testCase() {
-        // Input using In.java class
-        int n = In.readInt();
+        int n = In.readInt(); // number of cards
+        int m = In.readInt(); // cards we pick
 
-        // Binomial coefficient
-        // Algorithms.binom(n, k) returns 'n choose k'
+        DecimalFormat df = new DecimalFormat("#.########");
+        df.setMinimumFractionDigits(1);
+        df.setRoundingMode(RoundingMode.FLOOR);
+        Out.println(df.format(prob(n, m)));
+    }
 
-        // Factorial
-        // Algorithms.fact(n) returns 'n!'
-
-        // Output using Out.java class
-        Out.println(n + n);
+    private static double prob(int n, int m) {
+        /*
+            But you could write
+                ((n-m)*(n-m-1) + (n-m)*2 + (n-m+1)) / (n choose m)
+            the first term being all the (m-1) consecutive cards in the middle which can
+            have any of the remaining cards not adjacent to them (of which there are n-m-1);
+            the next term are the two (m-1) consecutive cards at the edge which can have any
+            of the remaining cards not adjacent to them (n-m) in their group and finally there
+            are (n-m+1) m-consecutive card groups
+         */
+        return (double)((n - (m - 2)) * (n - (m - 1)) - (n - (m - 1)))  / Algorithms.binom(n, m);
     }
 }
