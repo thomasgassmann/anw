2add0c7041242cce4d550e23211b1f0937e72ba9
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-07-28 21:18:21 +0200

    But low sell high
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- src/buy_low_sell_high/BuyLowSellHigh.java
+++ src/buy_low_sell_high/BuyLowSellHigh.java
@@ -1,5 +1,6 @@
 package buy_low_sell_high;
 
+import algorithms.Graph;
 import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,8 @@ import org.junit.jupiter.api.Test;
 public class BuyLowSellHigh {
     @Test
     public void main() {
-        In.open("src/buy_low_sell_high/public/test3.in");
-        Out.compareTo("src/buy_low_sell_high/public/test3.out");
+        In.open("src/buy_low_sell_high/public/test1.in");
+        Out.compareTo("src/buy_low_sell_high/public/test1.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -17,6 +18,25 @@ public class BuyLowSellHigh {
     }
 
     private static void testCase() {
+        int n = In.readInt();
+        int m = In.readInt();
 
+        var graph = new Graph(n);
+
+        int MAX = 10000;
+        for (int i = 0; i < m; i++) {
+            int u = In.readInt();
+            int v = In.readInt();
+            int c = In.readInt(); // 1 wood, -1 brick
+            graph.addEdge(u, v, c == 1 ? 1 : MAX);
+            graph.addEdge(v, u, c == 1 ? 1 : MAX);
+        }
+
+        int f = graph.computeMaximumFlow(0, n - 1);
+        if (f >= MAX) {
+            Out.println("RICH");
+        } else {
+            Out.println(f / 2);
+        }
     }
 }
