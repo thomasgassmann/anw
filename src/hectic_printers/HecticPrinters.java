c14d2de0a6b4e65bc77fc164b263b94cb4ce5d3a
Thomas Gassmann <thomas.gassmann@hotmail.com>
2022-07-29 22:46:07 +0200

    do it in dp
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- src/hectic_printers/HecticPrinters.java
+++ src/hectic_printers/HecticPrinters.java
@@ -9,31 +9,13 @@ import java.util.HashMap;
 public class HecticPrinters {
     @Test
     public void main() {
-        In.open("src/hectic_printers/public/test2.in");
-        Out.compareTo("src/hectic_printers/public/test2.out");
+        In.open("src/hectic_printers/public/test1.in");
+        Out.compareTo("src/hectic_printers/public/test1.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
             testCase();
         }
-    }
-
-    private static HashMap<Integer, HashMap<Integer, Double>> memo;
-
-    private static Double getmemo(int s, int m) {
-        if (!memo.containsKey(s) || !memo.get(s).containsKey(m)) {
-            return null;
-        }
-
-        return memo.get(s).get(m);
-    }
-
-    private static void memoize(int s, int m, double res) {
-        if (!memo.containsKey(s)) {
-            memo.put(s, new HashMap<>());
-        }
-
-        memo.get(s).put(m, res);
     }
 
     private static void testCase() {
@@ -45,28 +27,25 @@ public class HecticPrinters {
             p[i] = In.readDouble();
         }
 
-        memo = new HashMap<>();
-        double res = expected(p, 0, m);
-        Out.println(res);
-    }
-
-    private static double expected(double[] p, int start, int minutesLeft) {
-        if (start >= p.length || minutesLeft == 0) {
-            return 0;
+        if (m == 0) {
+            Out.println(0D);
+            return;
         }
 
-        if (minutesLeft == 1) {
-            return p[start];
+        // dp[i][j] = expected number of pages printed if we start
+        // from i and have j minutes left
+        double[][] dp = new double[n][m + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][1] = p[i];
         }
 
-        Double c = getmemo(start, minutesLeft);
-        if (c != null) {
-            return c;
+        for (int j = 2; j <= m; j++) {
+            for (int i = 0; i < n; i++) {
+                double last = i + 1 < n ? dp[i + 1][j - 1] : 0;
+                dp[i][j] = p[i] * (1 + last) + (1 - p[i]) * dp[i][j - 1];
+            }
         }
 
-        double res = p[start] * (1 + expected(p, start + 1, minutesLeft - 1)) +
-                (1 - p[start]) * expected(p, start, minutesLeft - 1);
-        memoize(start, minutesLeft, res);
-        return res;
+        Out.println(dp[0][m]);
     }
 }
