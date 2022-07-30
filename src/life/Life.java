be12b0face66d63183ccd5fdb4dd55a7fcc49187
Thomas Gassmann <thomas@gassmann.dev>
2022-07-30 19:53:35 +0200

    Life force
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/life/Life.java
+++ src/life/Life.java
@@ -1,5 +1,6 @@
 package life;
 
+import algorithms.Graph;
 import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,8 @@ import org.junit.jupiter.api.Test;
 public class Life {
     @Test
     public void main() {
-        In.open("src/life/public/sample.in");
-        Out.compareTo("src/life/public/sample.out");
+        In.open("src/life/public/test1.in");
+        Out.compareTo("src/life/public/test1.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -17,6 +18,34 @@ public class Life {
     }
 
     private static void testCase() {
-        
+        int n = In.readInt();
+        int m = In.readInt();
+
+        var graph = new Graph(2 * n + 2);
+        int SOURCE = 2 * n;
+        int SINK = 2 * n + 1;
+        int MAX = 10000;
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            int p = In.readInt();
+            int f = In.readInt();
+
+            graph.addEdge(i, i + n, MAX);
+
+            graph.addEdge(SOURCE, i, p);
+            graph.addEdge(i + n, SINK, f);
+
+            total += f;
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = In.readInt();
+            int v = In.readInt();
+
+            graph.addEdge(u, v + n, MAX);
+        }
+
+        var max = graph.computeMaximumFlow(SOURCE, SINK);
+        Out.println(max == total ? "yes" : "no");
     }
 }
