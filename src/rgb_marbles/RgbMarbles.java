28a2dad71d1e264f4101c10e748449b9c2f7776d
Thomas Gassmann <thomas@gassmann.dev>
2022-08-03 23:24:37 +0200

    Did rgb marbles
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/rgb_marbles/RgbMarbles.java
+++ src/rgb_marbles/RgbMarbles.java
@@ -1,14 +1,18 @@
 package rgb_marbles;
 
+import algorithms.Algorithms;
 import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class RgbMarbles {
     @Test
     public void main() {
-        In.open("src/rgb_marbles/public/sample.in");
-        Out.compareTo("src/rgb_marbles/public/sample.out");
+        In.open("src/rgb_marbles/public/test1.in");
+        Out.compareTo("src/rgb_marbles/public/test1.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -17,6 +21,25 @@ public class RgbMarbles {
     }
 
     private static void testCase() {
+        int r = In.readInt();
+        int g = In.readInt();
+        int b = In.readInt();
+        int total = r + b + g;
 
+        int m = In.readInt();
+
+        int x = In.readInt();
+        int y = In.readInt();
+        int z = In.readInt();
+
+        // (x choose r) * (y choose g) * (z choose b) / (total choose m)
+        long numerator = Algorithms.binom(r, x) * Algorithms.binom(g, y) * Algorithms.binom(b, z);
+        long denominator = Algorithms.binom(total, m);
+
+        double prob = numerator / (double)denominator;
+        DecimalFormat df = new DecimalFormat("#.########");
+        df.setMinimumFractionDigits(1);
+        df.setRoundingMode(RoundingMode.FLOOR);
+        Out.println(df.format(prob));
     }
 }
