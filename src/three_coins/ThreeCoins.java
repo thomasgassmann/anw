5499ec19ae2bc9e3ffa5d2ff3814492b936e745a
Thomas Gassmann <thomas@gassmann.dev>
2022-08-11 13:22:33 +0200

    three coins 3
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/three_coins/ThreeCoins.java
+++ src/three_coins/ThreeCoins.java
@@ -4,13 +4,14 @@ import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
 
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class ThreeCoins {
     @Test
     public void main() {
-        In.open("src/three_coins/public/sample.in");
-        Out.compareTo("src/three_coins/public/sample.out");
+        In.open("src/three_coins/public/test3.in");
+        Out.compareTo("src/three_coins/public/test3.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -28,6 +29,7 @@ public class ThreeCoins {
 
         var df = new DecimalFormat("#.#####");
         df.setMinimumFractionDigits(1);
+        df.setRoundingMode(RoundingMode.FLOOR);
         switch (x) {
             case 1:
                 Out.println(df.format(q1(p, a, b)));
@@ -44,7 +46,19 @@ public class ThreeCoins {
     }
 
     private static double q3(int n, double p, double a, double b) {
-        return 0;
+        // B: number of times beta coin used
+        // Pr[B > 0 | every time red at the end] = 1 - Pr[B == 0 | every time red at the end]
+        double alwaysRedOnlyAlpha = 1;
+        for (int i = 0; i < n; i++) {
+            alwaysRedOnlyAlpha *= p * a;
+        }
+
+        double alwaysRed = 1;
+        for (int i = 0; i < n; i++) {
+            alwaysRed *= p * a + (1 - p) * b;
+        }
+
+        return 1 - alwaysRedOnlyAlpha / alwaysRed;
     }
 
     private static double q2(double p, double a, double b) {
