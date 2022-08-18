d6badd0d58153c3acee18a31fd0da9351a88d392
Thomas Gassmann <thomas@gassmann.dev>
2022-08-18 21:24:22 +0200

    t1
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/two_boxes/TwoBoxes.java
+++ src/two_boxes/TwoBoxes.java
@@ -4,6 +4,9 @@ import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class TwoBoxes {
     @Test
     public void main() {
@@ -16,7 +19,35 @@ public class TwoBoxes {
         }
     }
 
+    private static double q1(int r1, int b1, int r2, int b2) {
+        return
+                r1 / (double)(r1 + b1) * (r2 + 1) / (r2 + 1.0 + b2)
+              + b1 / (double)(r1 + b1) * r2 / (r2 + 1.0 + b2);
+    }
+
     private static void testCase() {
-        
+        int r1 = In.readInt();
+        int b1 = In.readInt();
+        int r2 = In.readInt();
+        int b2 = In.readInt();
+
+        int n = In.readInt();
+        int x = In.readInt();
+
+        var d = new DecimalFormat("#.#####");
+        d.setMinimumFractionDigits(1);
+        switch (x) {
+            case 1:
+                Out.println(d.format(q1(r1, b1, r2, b2)));
+                break;
+            case 2:
+                Out.println('2');
+                break;
+            case 3:
+                Out.println('3');
+                break;
+            default:
+                break;
+        }
     }
 }
