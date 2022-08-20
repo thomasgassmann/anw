a05b2314497928b868f880468018002d739e7f25
Thomas Gassmann <thomas@gassmann.dev>
2022-08-20 13:40:57 +0200

    Toppling dominoes
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/toppling_dominoes/TopplingDominoes.java
+++ src/toppling_dominoes/TopplingDominoes.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 public class TopplingDominoes {
     @Test
     public void main() {
-        In.open("src/toppling_dominoes/public/sample.in");
-        Out.compareTo("src/toppling_dominoes/public/sample.out");
+        In.open("src/toppling_dominoes/public/test2.in");
+        Out.compareTo("src/toppling_dominoes/public/test2.out");
 
         int t = In.readInt();
         for (int i = 0; i < t; i++) {
@@ -17,6 +17,15 @@ public class TopplingDominoes {
     }
 
     private static void testCase() {
+        int n = In.readInt();
+        int fallUntil = In.readInt();
+        for (int i = 1; i < n; i++) {
+            int h = In.readInt();
+            if (i < fallUntil) {
+                fallUntil = Math.min(Math.max(fallUntil, i + h), n);
+            }
+        }
 
+        Out.println(fallUntil);
     }
 }
