fc6d2f60374a6668483b35c091d33e4e4d704900
Thomas Gassmann <thomas@gassmann.dev>
2022-09-27 00:31:55 +0200

    Init exam ce solutions
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/game_of_skill/GameOfSkill.java
+++ src/game_of_skill/GameOfSkill.java
@@ -16,7 +16,75 @@ public class GameOfSkill {
         }
     }
 
-    private void testCase() {
+    private static double q1(int n, double[] p) {
+        double res = 0;
+        for (int i = 2; i <= 6; i += 2) {
+            res += p[i - 1] * (1.0/6);
+        }
 
+        return res;
+    }
+
+    private static double q2(int n, double[] p) {
+        // Pr[first throw 3 | second throw 7] =
+        // Pr[first throw 3 and second throw 7] / Pr[second throw 7]
+        double secondThrow7 = 0;
+        for (int i = 1; i <= 6; i++) {
+            int secondThrow = 7 - i;
+            secondThrow7 += p[i - 1] * p[secondThrow - 1];
+        }
+
+        // first throw 3, then 4
+        double numerator = p[2] * p[3];
+        return numerator / secondThrow7;
+    }
+
+    private static double q3(int n, double[] p) {
+        // expected number of throws to reach n
+        double[] dp = new double[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i == n) {
+                for (int j = i - 1; j >= i - 6 && j >= 0; j--) {
+                    int minThrow = i - j;
+                    double totalReach = 0;
+                    for (int k = minThrow; k <= 6; k++) {
+                        dp[i] += (1 + dp[j]) * p[k - 1];
+                    }
+                }
+
+                continue;
+            }
+
+            for (int j = i - 1; j >= i - 6 && j >= 0; j--) {
+                int throwValue = i - j;
+                dp[i] += (1 + dp[j]) * p[throwValue - 1];
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void testCase() {
+        int n = In.readInt();
+        int q = In.readInt();
+        double[] p = new double[6];
+        for (int i = 0; i < 6; i++) {
+            p[i] = In.readDouble();
+        }
+
+        switch (q) {
+            case 1:
+                Out.println(q1(n, p));
+                break;
+            case 2:
+                Out.println(q2(n, p));
+                break;
+            case 3:
+                Out.println(q3(n, p));
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/hourglass/Hourglass.java
+++ src/hourglass/Hourglass.java
@@ -1,8 +1,12 @@
 package hourglass;
 
+import algorithms.Graph;
 import io.In;
 import io.Out;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class Hourglass {
     @Test
@@ -16,7 +20,87 @@ public class Hourglass {
         }
     }
 
-    private void testCase() {
+    private static boolean color(boolean[] c, boolean[] visited, LinkedList<Integer>[] edges, int start) {
+        visited[start] = true;
+        for (var edge : edges[start]) {
+            if (visited[edge] && c[start] == c[edge]) {
+                return false;
+            }
 
+            if (!visited[edge]) {
+                c[edge] = !c[start];
+                if (!color(c, visited, edges, edge)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private void testCase() {
+        int n = In.readInt();
+        int m = In.readInt();
+
+        LinkedList<Integer>[] graphEdges = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            graphEdges[i] = new LinkedList<Integer>();
+        }
+
+
+        var graph = new Graph(n + 2);
+        int SOURCE = n;
+        int SINK = n + 1;
+
+        int[] s = new int[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = In.readInt();
+        }
+
+        var weights = new HashMap<Integer, HashMap<Integer, Integer>>();
+
+        for (int i = 0; i < m; i++) {
+            int u = In.readInt();
+            int v = In.readInt();
+            int w = In.readInt();
+            graphEdges[u].add(v);
+            graphEdges[v].add(u);
+
+            if (!weights.containsKey(u)) {
+                weights.put(u, new HashMap<Integer, Integer>());
+            }
+
+            if (!weights.containsKey(v)) {
+                weights.put(v, new HashMap<Integer, Integer>());
+            }
+
+            weights.get(u).put(v, w);
+            weights.get(v).put(u, w);
+
+            graph.addEdge(u, v, w);
+            graph.addEdge(v, u, w);
+        }
+
+        boolean[] c = new boolean[n];
+        boolean[] visited = new boolean[n];
+        if (!color(c, visited, graphEdges, 0)) {
+            Out.println("no");
+            return;
+        }
+
+        int totalSandLow = 0;
+        int totalSandHigh = 0;
+        for (int i = 0; i < n; i++) {
+            if (!c[i]) {
+                graph.addEdge(i, SINK, s[i]);
+                totalSandLow += s[i];
+            } else {
+                graph.addEdge(SOURCE, i, s[i]);
+                totalSandHigh += s[i];
+            }
+        }
+
+        var flow = graph.computeMaximumFlow(SOURCE, SINK);
+        Out.println(flow >= Math.min(totalSandHigh, totalSandLow) ? "yes" : "no");
     }
 }
